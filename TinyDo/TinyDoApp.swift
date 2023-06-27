544c0a8dcc719b94d3e1844a99376dd582040860
EveSquare <50450074+EveSquare@users.noreply.github.com>
2023-06-27 23:08:34 +0900

    Initial Commit

---
+++ TinyDo/TinyDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyDoApp.swift
+//  TinyDo
+//
+//  Created by EveSquare on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct TinyDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
